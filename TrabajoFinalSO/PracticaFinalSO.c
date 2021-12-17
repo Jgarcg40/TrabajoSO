f7f4db7e92cd068a193dcac40ac55bc4fb516648
Jgarcg40 <jgarcg40@estudiantes.unileon.es>
2021-12-17 18:20:04 +0100

    carpetas y archivo inicial creado

---
+++ TrabajoFinalSO/PracticaFinalSO.c
@@ -0,0 +1,23 @@
+/* Librerias necesarias */
+# include <stdio.h>
+# include <stdlib.h>
+# include <ctype.h>
+# include <time.h>
+# include <pthread.h>
+# include <signal.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <errno.h>
+
+
+/* Funcion principal del programa. */
+int main(int argc, char const *argv[]){
+
+
+
+
+
+return 0;
+}
+
